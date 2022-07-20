7d829a451604e53aa7921763f4a435f1aab0fec9
dylanbrine <dylan.brine@milkmoovement.com>
2022-07-19 21:06:34 -0300

    oops

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ export const Index = () => {
   );
 };
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 render(
   <BrowserRouter>
     <Routes>
